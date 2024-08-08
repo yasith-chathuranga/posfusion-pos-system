78a630930c934144f373e3080c4a0fe6b6afe0a7
yasith-chathuranga <yasithchathuranga999@gmail.com>
2024-08-09 01:31:35 +0530

    create SQLUtil and refactor CustomerDaoImpl, ItemDaoImpl

--- back-end/src/main/java/com/example/backend/dao/custom/impl/CustomerDaoImpl.java
+++ back-end/src/main/java/com/example/backend/dao/custom/impl/CustomerDaoImpl.java
@@ -1,43 +1,28 @@
 package com.example.backend.dao.custom.impl;
 
 import com.example.backend.dao.custom.CustomerDao;
-import com.example.backend.db.ConnectionManager;
 import com.example.backend.entity.Customer;
+import com.example.backend.utill.SQLUtil;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerDaoImpl implements CustomerDao {
-
-    private Connection connection;
-    PreparedStatement pstm;
-
     @Override
     public boolean save(Customer customer) throws SQLException {
-        connection = ConnectionManager.getInstance().getConnection();
-
-        pstm = connection.prepareStatement("INSERT INTO customer VALUES (?,?,?,?)");
-        pstm.setString(1, customer.getId());
-        pstm.setString(2, customer.getName());
-        pstm.setString(3, customer.getAddress());
-        pstm.setDouble(4, customer.getSalary());
-
-        return pstm.executeUpdate() > 0;
-
+        return SQLUtil.execute("INSERT INTO customer VALUES (?,?,?,?)",
+                customer.getId(),
+                customer.getName(),
+                customer.getAddress(),
+                customer.getSalary()
+        );
     }
 
     @Override
     public Customer getData(String id) throws SQLException {
-        connection = ConnectionManager.getInstance().getConnection();
-
-        pstm = connection.prepareStatement("SELECT * FROM customer WHERE cust_id=?");
-        pstm.setString(1, id);
-
-        ResultSet resultSet = pstm.executeQuery();
+        ResultSet resultSet = SQLUtil.execute("SELECT * FROM customer WHERE cust_id=?", id);
         if (resultSet.next()) {
             return new Customer(
                     resultSet.getString(1),
@@ -51,33 +36,22 @@ public class CustomerDaoImpl implements CustomerDao {
 
     @Override
     public boolean update(Customer customer) throws SQLException {
-        connection = ConnectionManager.getInstance().getConnection();
-
-        pstm = connection.prepareStatement("UPDATE customer SET cust_name = ? , cust_address = ? , cust_salary = ? WHERE cust_id=?");
-        pstm.setString(1, customer.getName());
-        pstm.setString(2, customer.getAddress());
-        pstm.setDouble(3, customer.getSalary());
-        pstm.setString(4, customer.getId());
-
-        return pstm.executeUpdate() > 0;
+        return SQLUtil.execute("UPDATE customer SET cust_name = ? , cust_address = ? , cust_salary = ? WHERE cust_id=?",
+                customer.getName(),
+                customer.getAddress(),
+                customer.getSalary(),
+                customer.getId()
+        );
     }
 
     @Override
     public boolean delete(String id) throws SQLException {
-        connection = ConnectionManager.getInstance().getConnection();
-
-        pstm = connection.prepareStatement("DELETE FROM customer WHERE cust_id=?");
-        pstm.setString(1, id);
-
-        return pstm.executeUpdate() > 0;
+        return SQLUtil.execute("DELETE FROM customer WHERE cust_id=?", id);
     }
 
     @Override
     public List<Customer> getAll() throws SQLException {
-        connection = ConnectionManager.getInstance().getConnection();
-
-        pstm = connection.prepareStatement("SELECT * FROM customer");
-        ResultSet resultSet = pstm.executeQuery();
+        ResultSet resultSet = SQLUtil.execute("SELECT * FROM customer");
 
         List<Customer> customerList = new ArrayList<>();
 
@@ -89,7 +63,6 @@ public class CustomerDaoImpl implements CustomerDao {
                     resultSet.getDouble(4)
             ));
         }
-
         return customerList;
     }
 }
