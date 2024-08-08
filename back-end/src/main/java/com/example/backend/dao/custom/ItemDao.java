e90ce3b3db85d16abc931524ae6f57525610ca10
yasith-chathuranga <yasithchathuranga999@gmail.com>
2024-08-08 22:26:31 +0530

    item search developed

--- back-end/src/main/java/com/example/backend/dao/custom/ItemDao.java
+++ back-end/src/main/java/com/example/backend/dao/custom/ItemDao.java
@@ -1,9 +1,11 @@
 package com.example.backend.dao.custom;
 
+import com.example.backend.dto.ItemDto;
 import com.example.backend.entity.Item;
 
 import java.sql.SQLException;
 
 public interface ItemDao {
     boolean addItem(Item item) throws SQLException;
+    ItemDto searchItem(int id) throws SQLException;
 }
